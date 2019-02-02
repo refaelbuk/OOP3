60770264d8e73cfe707c2a2c3f060e7e9b646e5b
refaelbuk <46992576+refaelbuk@users.noreply.github.com>
2019-02-02 23:25:09 +0200

    Add files via upload

--- canvas.cpp
+++ canvas.cpp
@@ -1,11 +1,10 @@
+#include <math.h>
+#define _USE_MATH_DEFINES
+
 #include "canvas.h"
-#include "C_Line.h"
-#include "Circle.h"
-#include "Line.h"
-#include "Point.h"
-#include "Rectangle.h"
-#include "Shape.h"
+
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 ostream& operator<<(ostream& os, color c)
@@ -18,7 +17,7 @@ ostream& operator<<(ostream& os, color c)
 		case color::blue:		os << "Blue "; break;
 		case color::yellow:		os << "Yellow "; break;
 		case color::black:		os << "Black "; break;
-		case color::no_color:	os << "No color "; break:
+		default:				os << "No color ";
 	}
 	return os;
 }
@@ -29,5 +28,39 @@ ostream& operator<<(ostream& os, const Point& p)
 	return os;
 }
 
+void Line::draw() const
+{
+	cout << "No color Line from " << m_org << " To " << m_end << endl;;
+}
 
+double Line::length() const
+{
+	return sqrt((double)((m_org.x()-m_end.x())*(m_org.x()-m_end.x()) + (m_org.y()-m_end.y())*(m_org.y()-m_end.y())));
+}
 
+void C_Line::draw() const
+{
+	cout<< m_color << " Line from " << m_org << " To " << m_end <<endl;
+}
+
+void Circle::draw() const
+{
+	cout << m_color << " circle: Center in" << m_org << "Radius in " << m_radius << endl;
+}
+
+double Circle::area() const
+{
+	double sum = 0;
+	sum = pow((M_1_PI*m_radius), 2);
+	return sum;
+}
+
+void Circle::scale(double s)
+{
+	m_radius = s * m_radius;
+}
+
+void Circle::draw_to_file(ofstream& file) const
+{
+	file << m_color << " circle: Center in" << m_org << "Radius in " << m_radius << endl;
+}
